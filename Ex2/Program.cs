35f4bf6ad360e1c8802428f84218d77aab1be549
kaiquemauricio <113217451+kaiquemauricio@users.noreply.github.com>
2022-09-15 17:51:33 -0300

    Add files via upload

---
+++ Ex2/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+
+namespace POO2Ex2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("==Área do quadrado (aresta)==");
+
+            AreaQuadrado aq;
+            aq = new AreaQuadrado();
+
+            do
+            {
+                Console.Write("Digite o valor da Aresta: ");
+                aq.setAresta(double.Parse(Console.ReadLine()));
+            }
+            while (aq.getAresta() <= 0);
+
+            aq.calcular();
+
+            Console.WriteLine();
+            Console.WriteLine("Aresta: {0}", aq.getAresta());
+            Console.Write("Área do Quadrado = {0}", aq.getResultado());
+        }
+    }
+}
