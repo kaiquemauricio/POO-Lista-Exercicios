cbe12d1011937805d218e49e1383ccba7b862ca0
kaiquemauricio <113217451+kaiquemauricio@users.noreply.github.com>
2022-09-15 17:57:42 -0300

    Add files via upload

---
+++ Ex12/FTriangulo.cs
@@ -0,0 +1,102 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace POO2Ex12
+{
+    class FTriangulo
+    {
+        private int a;
+        private int b;
+        private int c;
+
+        #region construtores
+
+        public FTriangulo()
+        {
+            this.a = 0;
+            this.b = 0;
+            this.c = 0;
+        }
+
+        public FTriangulo(int a, int b, int c)
+        {
+            this.a = a;
+            this.b = b;
+            this.c = c;
+        }
+
+        #endregion
+
+        #region getset
+        public void setA(int x)
+        {
+            this.a = x;
+        }
+
+        public void setB(int x)
+        {
+            this.b = x;
+        }
+
+        public void setC(int x)
+        {
+            this.c = x;
+        }
+
+        public int getA()
+        {
+            return this.a;
+        }
+
+        public int getB()
+        {
+            return this.b;
+        }
+
+        public int getC()
+        {
+            return this.c;
+        }
+
+        #endregion
+
+        #region ifelse
+        public void triangular()
+        {
+            if (this.a + this.b < this.c)
+            {
+                Console.Write("Não formam triângulo");
+                return;
+            }
+            if (this.a + this.c < this.b)
+            {
+                Console.Write("Não formam triângulo");
+                return;
+            }
+            if (this.b + this.c < this.a)
+            {
+                Console.Write("Não formam triângulo");
+                return;
+            }
+
+            //tipo de triangulo
+            if (this.a == this.b && this.a == this.c)
+            {
+                Console.Write("Triângulo equilátero");
+                return;
+            }
+            if (this.a == this.b || this.b == this.c || this.a == this.c)
+            {
+                Console.Write("Triângulo isosceles");
+            }
+            else
+            {
+                Console.Write("Triângulo escaleno");
+            }
+
+
+        }
+        #endregion
+    }
+}
---
+++ Ex12/Program.cs
@@ -0,0 +1,40 @@
+﻿using System;
+
+namespace POO2Ex12
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("=-ABC Formação de triangulo==");
+            Console.WriteLine();
+
+            FTriangulo ft;
+            ft = new FTriangulo();
+
+            do
+            {
+                Console.Write("Digite o valor do lado A: ");
+                ft.setA(int.Parse(Console.ReadLine()));
+            }
+            while (ft.getA() <= 0);
+
+            do
+            {
+                Console.Write("Digite o valor do lado B: ");
+                ft.setB(int.Parse(Console.ReadLine()));
+            }
+            while (ft.getB() <= 0);
+
+            do
+            {
+                Console.Write("Digite o valor do lado C: ");
+                ft.setC(int.Parse(Console.ReadLine()));
+            }
+            while (ft.getC() <= 0);
+
+            Console.WriteLine();
+            ft.triangular();
+        }
+    }
+}
