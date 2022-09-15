8641733f01ee7d0ddd9dc258885a269464f19419
kaiquemauricio <113217451+kaiquemauricio@users.noreply.github.com>
2022-09-15 17:56:51 -0300

    Add files via upload

---
+++ Ex10/Program.cs
@@ -0,0 +1,43 @@
+﻿using System;
+
+namespace POO2Ex10
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("==Área de um retângulo, terreno grande-pequeno==");
+
+            Terreno t;
+            t = new Terreno();
+
+            do
+            {
+                Console.Write("Digite a base do retângulo: ");
+                t.setBasin(double.Parse(Console.ReadLine()));
+            }
+            while (t.getBasin() <= 0);
+
+            do
+            {
+                Console.Write("Digite a altura do retângulo: ");
+                t.setAltura(double.Parse(Console.ReadLine()));
+            }
+            while (t.getAltura() <= 0);
+
+            t.calcular();
+
+            Console.WriteLine();
+            Console.Write("Area = {0} ", t.getArea());
+            if (t.getArea() > 100)
+            {
+                Console.WriteLine("Terreno grande");
+            }
+            else
+            {
+                Console.WriteLine("Terreno pequeno");
+            }
+
+        }
+    }
+}
---
+++ Ex10/Terreno.cs
@@ -0,0 +1,61 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace POO2Ex10
+{
+    class Terreno
+    {
+        private double basin;
+        private double altura;
+        private double area;
+
+        #region construtores
+
+        public Terreno()
+        {
+            this.basin = 0;
+            this.altura = 0;
+        }
+
+        public Terreno(double basin, double altura)
+        {
+            this.basin = basin;
+            this.altura = altura;
+        }
+
+        #endregion
+
+        #region getset
+        public void setBasin(double x)
+        {
+            this.basin = x;
+        }
+
+        public void setAltura(double x)
+        {
+            this.altura = x;
+        }
+
+        public double getBasin()
+        {
+            return this.basin;
+        }
+
+        public double getAltura()
+        {
+            return this.altura;
+        }
+
+        public double getArea()
+        {
+            return this.area;
+        }
+        #endregion
+
+        public void calcular()
+        {
+            this.area = this.basin * this.altura;
+        }
+    }
+}
