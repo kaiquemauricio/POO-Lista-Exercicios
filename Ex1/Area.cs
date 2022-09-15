4dcf93a651703e584462e1d3b5a39cd1a3e82ba6
kaiquemauricio <113217451+kaiquemauricio@users.noreply.github.com>
2022-09-15 17:50:37 -0300

    Add files via upload

---
+++ Ex1/Area.cs
@@ -0,0 +1,59 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace POO2Ex1
+{
+    class Area
+    {
+        private int b1;
+        private int altura;
+        private int resultado;
+
+        #region constructors
+        public Area()
+        {
+            this.b1 = 0;
+            this.altura = 0;
+        }
+
+        public Area(int b1, int altura)
+        {
+            this.b1 = b1;
+            this.altura = altura;
+        }
+        #endregion
+
+        #region getset
+        public void setB1(int x)
+        {
+            this.b1 = x;
+        }
+
+        public void setAltura(int x)
+        {
+            this.altura = x; ;
+        }
+
+        public int getB1()
+        {
+            return this.b1;
+        }
+
+        public int getAltura()
+        {
+            return this.altura;
+        }
+
+        public int getResultado()
+        {
+            return this.resultado;
+        }
+        #endregion
+
+        public void calcular()
+        {
+            this.resultado = this.b1 * this.altura;
+        }
+    }
+}
---
+++ Ex1/Program.cs
@@ -0,0 +1,37 @@
+﻿using System;
+
+namespace POO2Ex1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            Area a;
+            a = new Area();
+
+            Console.WriteLine("==Calcular área do retângulo (base e altura)==");
+
+            do
+            {
+                Console.Write("Digite a Base: ");
+                a.setB1(int.Parse(Console.ReadLine()));
+            }
+            while (a.getB1() <= 0);
+
+            do
+            {
+                Console.Write("Digite a Altura: ");
+                a.setAltura(int.Parse(Console.ReadLine()));
+            }
+            while (a.getAltura() <= 0);
+
+            a.calcular();
+
+            Console.WriteLine();
+            Console.WriteLine("Base = {0}; Altura = {1}", a.getB1(), a.getAltura());
+            Console.Write("Área do retângulo = {0}", a.getResultado());
+        }
+    }
+}
+
