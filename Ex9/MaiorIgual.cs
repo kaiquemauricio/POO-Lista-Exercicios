5470c9de6233d48e834723cf7515a36e7fc93540
kaiquemauricio <113217451+kaiquemauricio@users.noreply.github.com>
2022-09-15 17:56:21 -0300

    Add files via upload

---
+++ Ex9/MaiorIgual.cs
@@ -0,0 +1,70 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace POO2Ex9
+{
+    class MaiorIgual
+    {
+        private int n1;
+        private int n2;
+
+        #region construtores
+
+        public MaiorIgual()
+        {
+            this.n1 = 0;
+            this.n2 = 0;
+        }
+
+        public MaiorIgual(int n1, int n2)
+        {
+            this.n1 = n1;
+            this.n2 = n2;
+
+        }
+
+        #endregion
+
+        #region getset
+        public void setn1(int x)
+        {
+            this.n1 = x;
+        }
+
+        public void setn2(int x)
+        {
+            this.n2 = x;
+        }
+
+        public int getn1()
+        {
+            return this.n1;
+        }
+
+        public int getn2()
+        {
+            return this.n2;
+        }
+        #endregion
+
+        public int indicarMaior()
+        {
+            if (this.n1 == this.n2)
+            {
+                return 0;
+            }
+            else
+            {
+                if (this.n1 > this.n2)
+                {
+                    return 1;
+                }
+                else
+                {
+                    return 2;
+                }
+            }
+        }
+    }
+}
---
+++ Ex9/Program.cs
@@ -0,0 +1,38 @@
+﻿using System;
+
+namespace POO2Ex9
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("==Maior de 2 números==");
+
+            MaiorIgual m;
+            m = new MaiorIgual();
+
+            Console.Write("Digite o primeiro número: ");
+            m.setn1(int.Parse(Console.ReadLine()));
+
+            Console.Write("Digite o segundo número: ");
+            m.setn2(int.Parse(Console.ReadLine()));
+
+            Console.WriteLine();
+            if (m.indicarMaior() == 0)
+            {
+                Console.WriteLine("Os numeros são idênticos");
+            }
+            else
+            {
+                if (m.indicarMaior() == 1)
+                {
+                    Console.WriteLine("Maior: {0} (primeiro número)", m.getn1());
+                }
+                else
+                {
+                    Console.WriteLine("Maior: {0} (segundo número)", m.getn2());
+                }
+            }
+        }
+    }
+}
