ba64658f790637ef9b7aa573e992d02ede0d4032
kaiquemauricio <113217451+kaiquemauricio@users.noreply.github.com>
2022-09-15 17:57:19 -0300

    Add files via upload

---
+++ Ex11/IMC.cs
@@ -0,0 +1,61 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace POO2Ex11
+{
+    class IMC
+    {
+        private double altura;
+        private double peso;
+        private double Imc;
+
+        #region construtores
+
+        public IMC()
+        {
+            this.altura = 0;
+            this.peso = 0;
+        }
+
+        public IMC(double altura, double peso)
+        {
+            this.altura = altura;
+            this.peso = peso;
+        }
+
+        #endregion
+
+        #region getset
+        public void setAltura(double x)
+        {
+            this.altura = x;
+        }
+
+        public void setPeso(double x)
+        {
+            this.peso = x;
+        }
+
+        public double getAltura()
+        {
+            return this.altura;
+        }
+
+        public double getPeso()
+        {
+            return this.peso;
+        }
+
+        public double getImc()
+        {
+            return this.Imc;
+        }
+        #endregion
+
+        public void calcular()
+        {
+            this.Imc = this.peso / Math.Pow(this.altura, 2);
+        }
+    }
+}
---
+++ Ex11/Program.cs
@@ -0,0 +1,51 @@
+﻿using System;
+
+namespace POO2Ex11
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("==IMC==");
+            Console.WriteLine();
+
+            IMC i;
+            i = new IMC();
+
+            do
+            {
+                Console.Write("Digite sua altura(0,00): ");
+                i.setAltura(double.Parse(Console.ReadLine()));
+            }
+            while (i.getAltura() > 2.7 || i.getAltura() < 0.6);
+
+            do
+            {
+                Console.Write("Digite seu peso(kg): ");
+                i.setPeso(double.Parse(Console.ReadLine()));
+            }
+            while (i.getPeso() > 600 || i.getPeso() < 27);
+
+            i.calcular();
+
+            Console.WriteLine();
+            Console.Write("IMC = {0} |", i.getImc());
+
+            if (i.getImc() < 20)
+            {
+                Console.WriteLine("Você está abaixo do peso");
+            }
+            else
+            {
+                if (i.getImc() >= 25)
+                {
+                    Console.WriteLine("Você está acima do peso");
+                }
+                else
+                {
+                    Console.WriteLine("Você está no seu peso ideal");
+                }
+            }
+        }
+    }
+}
