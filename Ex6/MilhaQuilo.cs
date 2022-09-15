bfa43ed7d8de11e48a4903c775fce0a04200f07d
kaiquemauricio <113217451+kaiquemauricio@users.noreply.github.com>
2022-09-15 17:54:10 -0300

    Add files via upload

---
+++ Ex6/MilhaQuilo.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace POO2Ex6
+{
+    class MilhaQuilo
+    {
+        private double milhas;
+        private double resultado;
+
+        #region construtores
+        
+        public MilhaQuilo()
+        {
+            this.milhas = 0;
+        }
+
+        public MilhaQuilo(double milhas)
+        {
+            this.milhas = milhas;
+        }
+
+        #endregion
+
+        #region getset
+        public void setMilhas(double x)
+        {
+            this.milhas = x;
+        }
+
+        public double getMilhas()
+        {
+            return this.milhas;
+        }
+
+        public double getResultado()
+        {
+            return this.resultado;
+        }
+        #endregion
+
+        public void calcular()
+        {
+            this.resultado = this.milhas * 1.852;
+        }
+    }
+}
---
+++ Ex6/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+
+namespace POO2Ex6
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("==Milhas marítmas para quilômetros==");
+
+            MilhaQuilo mq;
+            mq = new MilhaQuilo();
+
+            do
+            {
+                Console.Write("Digite a quantia em Milhas Marítmas: ");
+                mq.setMilhas(double.Parse(Console.ReadLine()));
+            }
+            while (mq.getMilhas() <= 0);
+
+            mq.calcular();
+
+            Console.WriteLine();
+            Console.WriteLine("{0} Milhas Marítmas = {1} Km", mq.getMilhas(), mq.getResultado());
+        }
+    }
+}
