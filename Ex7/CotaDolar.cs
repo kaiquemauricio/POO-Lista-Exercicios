c25d68515c39b4869ce6c375157ddfcc30e045d0
kaiquemauricio <113217451+kaiquemauricio@users.noreply.github.com>
2022-09-15 17:54:40 -0300

    Add files via upload

---
+++ Ex7/CotaDolar.cs
@@ -0,0 +1,62 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace POO2Ex7
+{
+    class CotaDolar
+    {
+        private double cotacao;
+        private double quantia;
+        private double resultado;
+
+        #region construtores
+
+        public CotaDolar()
+        {
+            this.cotacao = 0;
+            this.quantia = 0;
+        }
+
+        public CotaDolar(double cotacao, double quantia)
+        {
+            this.cotacao = cotacao;
+            this.quantia = quantia;
+        }
+
+        #endregion
+
+        #region getset
+
+        public void setCotacao(double x)
+        {
+            this.cotacao = x;
+        }
+
+        public void setQuantia(double x)
+        {
+            this.quantia = x;
+        }
+
+        public double getCotacao()
+        {
+            return this.cotacao;
+        }
+
+        public double getQuantia()
+        {
+            return this.quantia;
+        }
+
+        public double getResultado()
+        {
+            return this.resultado;
+        }
+        #endregion
+
+        public void calcular()
+        {
+            this.resultado = this.quantia * this.cotacao;
+        }
+    }
+}
---
+++ Ex7/Program.cs
@@ -0,0 +1,35 @@
+﻿using System;
+
+namespace POO2Ex7
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("==Dolar para Real==");
+
+            CotaDolar cd;
+            cd = new CotaDolar();
+
+            do
+            {
+                Console.Write("Digite a Cotação do dolar atual: ");
+                cd.setCotacao(double.Parse(Console.ReadLine()));
+            }
+            while (cd.getCotacao() <= 0);
+
+            do
+            {
+                Console.Write("Digite o valor em dolar (00,00): ");
+                cd.setQuantia(double.Parse(Console.ReadLine()));
+            }
+            while (cd.getQuantia() <= 0);
+
+            cd.calcular();
+
+            Console.WriteLine();
+            Console.WriteLine("Cotação: {0}; Quantia: ${1}", cd.getCotacao(), cd.getQuantia().ToString("##00.00"));
+            Console.Write("Quantia em Real = R${0}", cd.getResultado().ToString("##00.00"));
+        }
+    }
+}
